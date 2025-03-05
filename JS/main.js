2ede475579f26de3f37d2b73050dbdade188e3e2
JohnCoffeeXL <jagustin.avalos74@gmail.com>
2025-03-05 16:45:30 -0300

    Backdrop Filter Bug with Navbar solved

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,9 @@
 const navToggler = document.querySelector('#open-btn');
 const navCloser = document.querySelector('#close-btn');
 const navList = document.querySelector('.nav-list');
-const joinBtn = document.querySelector('#join-btn');
 
 function toggleNav(isOpen) {
     navList.classList.toggle("active", isOpen);
-    joinBtn.classList.toggle("noblur", isOpen);
     navCloser.classList.toggle("active", isOpen);
     navToggler.classList.toggle("unactive", isOpen);
 }
