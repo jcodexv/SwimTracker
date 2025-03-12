315720ce769e287c7faafa711c48c8ff1ecf86fa
JohnCoffeeXL <jagustin.avalos74@gmail.com>
2025-03-12 16:01:54 -0300

    Website’s first page fully responsive

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,13 @@
 const navToggler = document.querySelector('#open-btn');
 const navCloser = document.querySelector('#close-btn');
 const navList = document.querySelector('.nav-list');
+const brand = document.querySelector('.brand');
 
 function toggleNav(isOpen) {
     navList.classList.toggle("active", isOpen);
     navCloser.classList.toggle("active", isOpen);
     navToggler.classList.toggle("unactive", isOpen);
+    brand.classList.toggle("hidden", isOpen);
 }
 
 navToggler.addEventListener('click', () => toggleNav(true));
